fcba88bc5da06c2061d7a73f26c783927e63b072
ashkaushik0007 <ashkaushik0007@gmail.com>
2017-03-30 22:26:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,22 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var http = require('http');
-var server = http.createServer(app);
+var express = require('express')
+  
+, http = require('http')
+  
+, app = express()
+  
+, server = http.createServer(app);
 
-//var index = require('./routes/index');
-//var tasks = require('./routes/tasks');
 
-var app = express();
-
-//View Engine
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//app.engine('html', require('ejs').renderFile);
-
-// Set Static Folder
-//app.use(express.static(path.join(__dirname, 'client')));
-
-// Body Parser MW
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
-
-//app.use('/', index);
-//app.use('/api', tasks);
 
 app.get('/',function(req,res){
   
-    res.sendFile('index.html');
-    console.log('hello world');
+res.sendFile('index.html');
 
 });
+
 
 server.listen(8080,'10.128.0.3',function(){
-    console.log('server started at 10.128.0.3:8080');
-});
 
+console.log('server started at 10.128.0.3:8080');
+
+);
