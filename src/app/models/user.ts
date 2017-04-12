380a8c0e3e9c1573b0d37491063b88afd6895fc2
Aashish Kaushik <ashaushik0007@github.com>
2017-04-12 09:17:02 +0530

    'sc'

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,7 +1,19 @@
 export class User {
-    _id: string;
-    username: string;
+    _id: string;        
+    email: string;
     password: string;
     firstName: string;
     lastName: string;
+    username: String;
+    emailVerified:boolean;
+    phoneVerified:boolean;
+    Role: { 
+            Name:String;
+            InvitedBy:string;
+            Email:String;
+          }
+    active:boolean;
+    phone: number;
+    created: Date;
+    updated: Date;
 }
