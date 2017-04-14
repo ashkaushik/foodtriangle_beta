febb3ade389dd2110a56360eb3a5796e77a32e19
Aashish Kaushik <ashaushik0007@github.com>
2017-04-14 12:50:48 +0530

    'sc'

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/',function(req, res, next){
-//     res.render('index.html');
-// });
-
-
-router.get('/*', function (req, res, next) {
-       if (/.js|.html|.css|templates|js|scripts/.test(req.path) || req.xhr) {
-       return next({ status: 404, message: 'Not Found' });
-       } else {
-          return res.render('index.html');
-       }
+/* GET api listing. */
+router.get('/',function(req, res, next){
+    res.render('index.html');
 });
 
 module.exports = router;
