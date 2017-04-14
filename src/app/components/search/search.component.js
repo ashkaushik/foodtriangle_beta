657ebec7f3f1380646de3e310cc46c81fe4de72d
Aashish Kaushik <ashaushik0007@github.com>
2017-04-14 19:20:40 +0530

    search component added

--- src/app/components/search/search.component.js
+++ src/app/components/search/search.component.js
@@ -5,18 +5,37 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var index_1 = require("../../services/index");
 var SearchComponent = (function () {
-    function SearchComponent() {
+    function SearchComponent(userService) {
+        this.userService = userService;
+        this.users = [];
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
+    SearchComponent.prototype.ngOnInit = function () {
+        this.loadAllUsers();
+    };
+    SearchComponent.prototype.deleteUser = function (_id) {
+        var _this = this;
+        this.userService.delete(_id).subscribe(function () { _this.loadAllUsers(); });
+    };
+    SearchComponent.prototype.loadAllUsers = function () {
+        var _this = this;
+        this.userService.getAll().subscribe(function (users) { _this.users = users; });
+    };
     return SearchComponent;
 }());
 SearchComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         templateUrl: 'search.component.html',
-    })
+    }),
+    __metadata("design:paramtypes", [index_1.UserService])
 ], SearchComponent);
 exports.SearchComponent = SearchComponent;
 //# sourceMappingURL=search.component.js.map
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input, Output } from '@angular/core';
+ 
+import { User } from '../../models/index';
+import { UserService } from '../../services/index';
 @Component({
   moduleId: module.id,
   templateUrl: 'search.component.html',
 })
 
-export class SearchComponent { }
+export class SearchComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+ 
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+ 
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+ 
+    deleteUser(_id: string) {
+        this.userService.delete(_id).subscribe(() => { this.loadAllUsers() });
+    }
+ 
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
