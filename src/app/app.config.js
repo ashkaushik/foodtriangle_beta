33662c992bf34cdbdeeabd922f5eb32dfcdcd7c9
Aashish Kaushik <ashaushik0007@github.com>
2017-04-16 18:44:26 +0530

    searchlist

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var AppConfig = (function () {
     function AppConfig() {
-        //public readonly apiUrl = 'http://104.154.215.211:8080';
-        this.apiUrl = 'http://127.0.0.1:8080';
+        this.apiUrl = 'http://104.154.215.211:8080';
+        //  public readonly apiUrl = 'http://127.0.0.1:8080';
     }
     return AppConfig;
 }());
