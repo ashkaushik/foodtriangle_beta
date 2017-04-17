97a9c7d3f8c0dd463c98754c0855d7085db52afc
ashkaushik <aashish.kaushik@BIRLASOFT.COM>
2017-04-17 21:11:42 +0530

    small change

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
- 
+
 import { User } from '../../models/index';
 import { UserService } from '../../services/index';
- 
+
 import { Menu } from '../../models/index';
 import { MenuService } from '../../services/index';
 @Component({
@@ -13,25 +13,25 @@ import { MenuService } from '../../services/index';
 export class SearchComponent implements OnInit {
     currentUser: User;
     users: User[] = [];
-    items: Menu[] = [];  
-    loading = true;  
+    items: Menu[] = [];
+    loading = true;
 
     constructor(private menuService:MenuService,private userService:UserService) {
-       // this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+       this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
- 
+
     ngOnInit() {
         this.loadAllUsers();
         this.loadsearchItems();
     }
- 
+
     deleteUser(_id: string) {
         this.userService.delete(_id).subscribe(() => { this.loadAllUsers() });
     }
- 
-    private loadAllUsers() { 
+
+    private loadAllUsers() {
         this.userService.getAll().subscribe(users => { this.users = users; });
-        this.loading = false;        
+        this.loading = false;
     }
 
     private loadsearchItems() {
